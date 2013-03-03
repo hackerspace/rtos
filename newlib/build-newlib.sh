492d93441274c9404dba64f4fd1ed3cdf2de01f8
Richard Marko <rmarko@fedoraproject.org>
2013-03-03 22:32:38 +0100

    fix build-newlib.sh

--- newlib/build-newlib.sh
+++ newlib/build-newlib.sh
@@ -16,6 +16,7 @@ if [ ! -a newlib-1.19-0 ]
   tar -xzf newlib-1.19.0.tar.gz
 fi
 
+mkdir build
 cd build
 ../newlib-1.19.0/configure --target=arm-none-eabi --disable-newlib-supplied-syscalls --prefix=`cd ../usr;pwd`
 make && make install
