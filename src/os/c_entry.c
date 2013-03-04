03b793a3071c07aa238e1873799adddad464d740
Maros Zatko <mzatko@redhat.com>
2013-03-04 01:18:11 +0100

    fill handler tbl

--- src/os/c_entry.c
+++ src/os/c_entry.c
@@ -1,6 +1,17 @@
 #include <c_entry.h>
+#include <string.h>
+
+typedef void (*fp)(void);
+extern fp _reset;
 
 void c_entry(void) {
+  fp tbl[20];
+
+  tbl[0] = (fp)0x00004000;
+  tbl[1] = _reset;
+
+  memcpy(0x0, tbl, sizeof(fp)*20);
+
   for (;;) {
   }
 }
