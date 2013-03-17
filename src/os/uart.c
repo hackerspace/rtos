64e088de3e3f315b0f35583ae3c191aadd933838
Maros Zatko <mzatko@redhat.com>
2013-03-17 02:54:17 +0100

    mockable sysputc + nonobvious mixup of init code

--- src/os/uart.c
+++ src/os/uart.c
@@ -1,8 +1,118 @@
+#include <system.h>
 #include <uart.h>
+#define GET32(x) (*(int*)(x))
+#define PUT32(x,y) (*(int*)(x)) = y
 
-void sysputc(char mychar) {
-  #define UART0_DATA ((volatile unsigned long *)(0x4000C000))
-  #define UART0_FLAG ((volatile unsigned long *)(0x4000C018))
+#define GET16(x) (*(short*)(x))
+#define PUT16(x,y) (*(short*)(x)) = y
+
+
+#define RCCBASE 0x40023800
+#define RCC_CR (RCCBASE+0x00)
+#define RCC_PLLCFGR (RCCBASE+0x04)
+#define RCC_CFGR (RCCBASE+0x08)
+#define RCC_AHB1ENR (RCCBASE+0x30)
+#define RCC_APB1ENR (RCCBASE+0x40)
+#define GPIODBASE 0x40020C00
+#define TIM5BASE 0x40000C00
+#define FLASH_ACR 0x40023C00
+#define GPIOABASE 0x40020000
+#define GPIOA_MODER (GPIOABASE+0x00)
+
+#ifdef __STM_
+
+#define USART2_BASE 0x40004400
+#define USART2_SR (USART2_BASE+0x00)
+#define USART2_DR (USART2_BASE+0x04)
+#define USART2_BRR (USART2_BASE+0x08)
+#define USART2_CR1 (USART2_BASE+0x0C)
+#define USART2_CR2 (USART2_BASE+0x10)
+#define USART2_CR3 (USART2_BASE+0x14)
+#define USART2_GTPR (USART2_BASE+0x18)
+#define GPIOA_AFRL (GPIOABASE+0x20)
+#define GPIOA_OTYPER (GPIOABASE+0x04)
+
+#endif
+
+#define USART2_BASE 0x4000e000
+#define USART2_SR (USART2_BASE+0x04)
+#define USART2_DR (USART2_BASE+0x00)
+#define USART2_BRR (USART2_BASE+0x08)
+#define USART2_CR1 (USART2_BASE+0x0C)
+#define USART2_CR2 (USART2_BASE+0x10)
+#define USART2_CR3 (USART2_BASE+0x14)
+#define USART2_GTPR (USART2_BASE+0x18)
+#define GPIOA_AFRL (GPIOABASE+0x20)
+#define GPIOA_OTYPER (GPIOABASE+0x04)
+
+
+
+//-------------------------------------------------------------------
+int uart_init ( void )
+{
+    unsigned int ra;
+
+    ra=GET32(RCC_AHB1ENR);
+    ra|=1<<0; //enable port A
+    PUT32(RCC_AHB1ENR,ra);
+
+    ra=GET32(RCC_APB1ENR);
+    ra|=1<<17; //enable USART2
+    PUT32(RCC_APB1ENR,ra);
+
+    //PA2 USART2_TX
+    //PA3 USART2_RX
+
+    ra=GET32(GPIOA_MODER);
+    ra|= (2<<4);
+    ra|= (2<<6);
+    PUT32(GPIOA_MODER,ra);
+    ra=GET32(GPIOA_OTYPER);
+    ra&=(1<<2);
+    ra&=(1<<3);
+    PUT32(GPIOA_OTYPER,ra);
+    ra=GET32(GPIOA_AFRL);
+    ra|=(7<<8);
+    ra|=(7<<12);
+    PUT32(GPIOA_AFRL,ra);
+
+    //42000000/16 = 2625000
+    //2625000/115200 = 22.786...
+    //.786... * 16 = 12.58
+    //12/16 = 0.75
+    //13/16 = 0.8125
+    //2625000/22.75 = 115384 184 .16%
+    //2625000/22.8125 = 115068 132 .11%
+    PUT32(USART2_BRR,(22<<4)|(13<<0));
+    PUT32(USART2_CR1,(1<<13)|(1<<3)|(1<<2));
+    return(0);
+}
+//-------------------------------------------------------------------
+void uart_putc ( unsigned int x )
+{
+    while (( GET32(USART2_SR) & (1<<7)) == 0) continue;
+    PUT32(USART2_DR,x);
+}
+//-------------------------------------------------------------------
+unsigned int uart_getc ( void )
+{
+    while (( GET32(USART2_SR) & (1<<5)) == 0) continue;
+    return(GET32(USART2_DR));
+}
+
+
+
+void sysputc___(char mychar) {uart_putc(mychar);};
+
+void sysputc(char mychar) {toggle_led(0b100);};
+
+void sysputc__(char c) {};
+
+void sysputc_(char mychar) {
+  //#define UART0_DATA ((volatile unsigned long *)(0x4000C000))
+  //#define UART0_FLAG ((volatile unsigned long *)(0x4000C018))
+  #define UART0_DATA ((volatile unsigned long *)(0x4000C004))
+  #define UART0_FLAG ((volatile unsigned long *)(0x4000C000))
   while ((*UART0_FLAG & 0x20) != 0);
   *UART0_DATA = mychar;
   return;
--- src/os/uart.h
+++ src/os/uart.h
@@ -2,5 +2,6 @@
 #define _UART_H_
 
 void sysputc(char mychar);
+int uart_init ( void );
 
 #endif
