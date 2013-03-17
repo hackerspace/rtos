f9002f5d31a8c922da25987cc5125d9ab1ddd4e7
Maros Zatko <mzatko@redhat.com>
2013-03-17 03:00:45 +0100

    better stack setup after reset, some minor hacks on fault hnds

--- src/os/reset.s
+++ src/os/reset.s
@@ -1,10 +1,10 @@
-#include<c_entry.h>
+#include<system.h>
 
 .cpu cortex-m3
 .syntax unified
 .thumb
 
-.word   0x20002000  /* stack top address */
+.word   0x20008000  /* stack top address */
 .word   _start      /* 1 Reset */
 .word   hang2       /* 2 NMI */
 .word   hang3       /* 3 HardFault */
@@ -19,14 +19,14 @@
 .word   hang7       /* 12 Debug Monitor */
 .word   hang        /* 13 RESERVED */
 .word   _ctx_switcher        /* 14 PendSV */
-.word   systick        /* 15 SysTick */
+.word   _systick        /* 15 SysTick */
 .word   hang        /* 16 External Interrupt(0) */
 .word   hang        /* 17 External Interrupt(1) */
 .word   hang        /* 18 External Interrupt(2) */
 .word   hang        /* 19 ...   */
 
 .global __memtop
-__memtop:         .word   0x20002000
+__memtop:         .word   0x20008000
 .global __psp_stack_size
 __psp_stack_size: .word   0x100
 .global __msp_stack_size
@@ -38,33 +38,53 @@ _start:
 
     ldr r1, __memtop      /* Top of memory */
 
-    ldr r2, __msp_stack_size
-    sbc r1, r1, r2
+/*    ldr r2, __msp_stack_size */
+    sbc r1, r1, #0x100
 
     msr psp, r1
     /* And finally subtract Kernel stack size to get final __memtop */
-    ldr r2, __psp_stack_size
-    sbc r1, r1, r2
+/*    ldr r2, __psp_stack_size */
+    sbc r1, r1, #0x100
+    ldr r0, __memtop
+    msr msp, r0
 
+/*    ldr r0, .Lbss_start
+    mov r1, #0
+    ldr r2, __memtop
+    sub r2, r2, r0
+    bl memset*/
 
+/*    mov r0, sp
+    push {r0}*/
     bl c_entry
+/*    pop {r0}
+    mov sp, r0*/
 /*    ldr r0, __memtop*/
 /*    msr psp, r0*/
 
-    mov r1, #0
+/*    mov r1, #0
     msr    control, r1      /* Change the mode */
-    isb
+/*    isb*/
 
     bl sys_sleep
     b .
 .Lc_entry:    .word c_entry
+.Lbss_start:    .word __bss_start__
+.Lbss_end:    .word __bss_end__
 
 faultstat: .word 0xe000ed28
 
 .thumb_func
 hang2:
+  mov r0, #0xfffffffd
+  bx r0
   b hang
 hang3:
+  MRS     R0,PSP                  /* Read PSP */
+  LDR     R1,[R0,#24]             /* Read Saved PC from Stack */
+  BKPT #0
+  mov r0, #0xfffffffd
+  bx r0
   b hang
 hang4:
   b hang
